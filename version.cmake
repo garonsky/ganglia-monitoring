358eecf9ab5366aa7b0d8b5c0482aa2ae88ffa44
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2013-09-17 10:17:54 -0400

    - ESP Ganglia Web Service
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

---
+++ version.cmake
@@ -0,0 +1,10 @@
+###
+## Version Information
+###
+set ( HPCC_PROJECT "community" )
+set ( HPCC_MAJOR 5 )
+set ( HPCC_MINOR 2 )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "closedown" )
+set ( HPCC_SEQUENCE 0 )
+###
